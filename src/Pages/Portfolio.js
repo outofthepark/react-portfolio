30bc0073960827a9c804d161eb1637e69419e7fc
Allison Harden <harden.allison@gmail.com>
2021-09-08 10:58:09 -0600

    Update to img src to try and get imagess to display in gh-pages deployment.

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -7,37 +7,37 @@ const Portfolio = () => {
       title: "Lorenz System Modeler",
       github: "https://github.com/outofthepark/LorenzSystem",
       deployment: "https://colab.research.google.com/drive/1ii7PEti0lONfEMEQXJ8jpgrlxPQwkO_x?usp=sharing",
-      img: "/lorenz-system.png"
+      img: "lorenz-system.png"
     },
     {
       title: "Tech Blog",
       github: "https://github.com/outofthepark/tech-blog",
       deployment: "https://agile-temple-92129.herokuapp.com/",
-      img: "/tech-blog.png"
+      img: "tech-blog.png"
     },
     {
       title: "Weather App",
       github: "https://github.com/outofthepark/WeatherApp",
       deployment: "https://outofthepark.github.io/WeatherApp/",
-      img: "/weather-app.png"
+      img: "weather-app.png"
     },
     {
       title: "Note Taker",
       github: "https://github.com/outofthepark/notetaker",
       deployment: "https://blooming-anchorage-66352.herokuapp.com/notes",
-      img: "/note-taker.png"
+      img: "note-taker.png"
     },
     {
       title: "Coding Quiz",
       github: "https://github.com/outofthepark/codequiz",
       deployment: "https://outofthepark.github.io/codequiz/",
-      img: "/coding-quiz.png"
+      img: "coding-quiz.png"
     },
     {
       title: "Portfolio (You are here!)",
       github: "https://github.com/outofthepark/react-portfolio",
       deployment: "http://outofthepark.github.io/react-portfolio",
-      img: "/react-portfolio.png"
+      img: "react-portfolio.png"
     }
   ]
     return (
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
       <footer className="footer">
           <nav className="footer-nav">
-              <a href="https://github.com/outofthepark" target="_blank" rel="noopener noreferrer"><img src="/github-icon.png" alt="Github" /></a>
-              <a><img src="/linkedin-icon.png" alt="LinkedIn" /></a>
+              <a href="https://github.com/outofthepark" target="_blank" rel="noopener noreferrer"><img src="github-icon.png" alt="Github" /></a>
+              <a><img src="linkedin-icon.png" alt="LinkedIn" /></a>
           </nav>
       </footer>
     );
